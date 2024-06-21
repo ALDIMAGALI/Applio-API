b00d12a557a1ad2ae90af80b61f3b14a21186544
ALDIMAGALI <AMROBLEDO@TECO.COM.AR>
2024-06-21 04:42:42 -0300

    actualiza

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import express from 'express'
-import modelsRouter from './routes/models'
-import blogRouter from './routes/blog'
-import userRouter from './routes/user'
+import express from 'express';
+import modelsRouter from './routes/models';
+import blogRouter from './routes/blog';
+import userRouter from './routes/user';
 const cors = require('cors');
 require('dotenv').config();
 
@@ -13,16 +13,15 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 declare global {
   namespace Express {
     interface Request {
-      apiKey: string; 
+      apiKey: string;
     }
   }
 }
 
-
-const app = express()
+const app = express();
 app.use(express.json(), cors());
 
-const PORT = 6969
+const PORT = 6969;
 
 app.get('/ping', async (_req, res) => {
   const start = Date.now();
@@ -36,63 +35,61 @@ app.get('/ping', async (_req, res) => {
 });
 
 app.use('/key=:apiKey/models', async (req, res, next) => {
-    const apiKey = req.params.apiKey;
-  
-    const { data: tokens, error } = await supabase
-      .from('tokens')
-      .select('token')
-      .eq('token', apiKey);
-  
-    if (error) {
-      res.status(500).json({ error: 'Bad API KEY' });
-    } else if (tokens.length === 0) {
-      res.status(401).json({ error: 'API KEY Not valid' });
-    } else {
-      req.apiKey = apiKey;
-      next();
-    }
-  }, modelsRouter);
+  const apiKey = req.params.apiKey;
 
-  app.use('/key=:apiKey/blog', async (req, res, next) => {
-    const apiKey = req.params.apiKey;
-  
-    const { data: tokens, error } = await supabase
-      .from('tokens')
-      .select('token')
-      .eq('token', apiKey);
-  
-    if (error) {
-      res.status(500).json({ error: 'Bad API KEY' });
-    } else if (tokens.length === 0) {
-      res.status(401).json({ error: 'API KEY Not valid' });
-    } else {
-      req.apiKey = apiKey;
-      next();
-    }
-  }, blogRouter);
+  const { data: tokens, error } = await supabase
+    .from('tokens')
+    .select('token')
+    .eq('token', apiKey);
 
-  app.use('/key=:apiKey/user', async (req, res, next) => {
-    const apiKey = req.params.apiKey;
-  
-    const { data: user, error } = await supabase
-      .from('tokens')
-      .select('role')
-      .eq('token', apiKey);
-  
-    if (error) {
-      res.status(500).json({ error: 'Bad API KEY' });
-    } else if (user.length === 0) {
-      res.status(401).json({ error: 'API KEY Not valid' });
-    } else if (user[0].role !== 'admin') {
-      res.status(403).json({ error: 'You need an API with permissions to perform this action' });
-    } else {
-      req.apiKey = apiKey;
-      next();
-    }
-  }, userRouter);
+  if (error) {
+    res.status(500).json({ error: 'Bad API KEY' });
+  } else if (tokens.length === 0) {
+    res.status(401).json({ error: 'API KEY Not valid' });
+  } else {
+    req.apiKey = apiKey;
+    next();
+  }
+}, modelsRouter);
 
-  
+app.use('/key=:apiKey/blog', async (req, res, next) => {
+  const apiKey = req.params.apiKey;
+
+  const { data: tokens, error } = await supabase
+    .from('tokens')
+    .select('token')
+    .eq('token', apiKey);
+
+  if (error) {
+    res.status(500).json({ error: 'Bad API KEY' });
+  } else if (tokens.length === 0) {
+    res.status(401).json({ error: 'API KEY Not valid' });
+  } else {
+    req.apiKey = apiKey;
+    next();
+  }
+}, blogRouter);
+
+app.use('/key=:apiKey/user', async (req, res, next) => {
+  const apiKey = req.params.apiKey;
+
+  const { data: user, error } = await supabase
+    .from('tokens')
+    .select('role')
+    .eq('token', apiKey);
+
+  if (error) {
+    res.status(500).json({ error: 'Bad API KEY' });
+  } else if (user.length === 0) {
+    res.status(401).json({ error: 'API KEY Not valid' });
+  } else if (user[0].role !== 'admin') {
+    res.status(403).json({ error: 'You need an API with permissions to perform this action' });
+  } else {
+    req.apiKey = apiKey;
+    next();
+  }
+}, userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
